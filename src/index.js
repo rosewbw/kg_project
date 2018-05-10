4175f47162e12efd263e5ae1f7ffef313e3972d6
DESKTOP-G2JJD5K\wbw <21680560@qq.com>
2018-05-10 09:43:30 +0800

    修补登陆验证不跳转的bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {
@@ -7,9 +7,9 @@ import {
     Redirect,
     withRouter
 } from "react-router-dom";
-import { App } from './editorPage';
+import {App} from './editorPage';
 import registerServiceWorker from './registerServiceWorker';
-import { Reg, Login } from './registerandlogin';
+import {Reg, Login} from './registerandlogin';
 import LearningPage from './learningPage';
 
 class IndexPage extends Component {
@@ -25,31 +25,10 @@ class IndexPage extends Component {
         this.logIn = this.logIn.bind(this);
         this.getToken = this.getToken.bind(this);
 
-        const token = this.getToken();
 
-        // 根据 Token 获取用户信息并存入
-        if (token) {
-            fetch('/fetchUserInfoWithToken', {
-                method: 'GET',
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": token,
-                },
-                // qs: { token }
-            }).then(res => res.json())
-                .then(res => {
-                    if (res && res.status === 'success') {
-                        this.setState({
-                            username: res.data.username,
-                            usertype: res.data.usertype,
-                            loginChecked: true,
-                        });
-                    }
-                });
-        }
     }
 
-    getToken(){
+    getToken() {
         return localStorage.getItem('token');
     }
 
@@ -62,43 +41,68 @@ class IndexPage extends Component {
             headers: {
                 "Content-Type": "application/json",
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 username: username,
                 password: password,
             })
-        }).then( res => res.json() ).then(res => {
+        }).then(res => res.json()).then(res => {
             console.log(res);
 
-            if(res.status === 'success'){
+            if (res.status === 'success') {
                 const options = res.data;
 
                 localStorage.setItem('token', options.token);
-                this.setState({ username,
+                this.setState({
+                    username,
                     usertype: options.usertype,
                 });
                 this.props.history.push('/');
 
 
-            }else{
+            } else {
                 alert("登录失败，请重新登录");
                 this.props.history.push('login');
             }
 
         })
     }
+    componentDidMount(){
+        const token = this.getToken();
 
+        // 根据 Token 获取用户信息并存入
+
+        fetch('/fetchUserInfoWithToken', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token,
+            },
+            // qs: { token }
+        }).then(res => res.json())
+            .then(res => {
+                if (res && res.status === 'success') {
+                    this.setState({
+                        username: res.data.username,
+                        usertype: res.data.usertype,
+                        loginChecked: true,
+                    });
+                }
+                this.setState({
+                    loginChecked: true,
+                });
+            });
+    }
     render() {
         return (
             <div>
                 <Route exact path="/"
                        render={() => {
                            const token = localStorage.getItem('token');
-                           const { usertype, loginChecked } = this.state;
-
+                           const {usertype, loginChecked} = this.state;
                            if (token && usertype) {
                                return usertype === 'student'
                                    ? <Redirect to={"/learning-page"}/>
-                                   : <Redirect to={"/editor-page"} />
+                                   : <Redirect to={"/editor-page"}/>
                            } else {
                                return loginChecked
                                    ? <Redirect to={"/login"}/>
@@ -113,7 +117,7 @@ class IndexPage extends Component {
                            />
                        )}
                 />
-                <Route path="/reg" component={Reg} />
+                <Route path="/reg" component={Reg}/>
                 <Route path="/editor-page"
                        render={(props) => (
                            <App username={this.state.username}
@@ -121,7 +125,7 @@ class IndexPage extends Component {
                            />
                        )}
                 />
-                <Route path="/learning-page" component={LearningPage} />
+                <Route path="/learning-page" component={LearningPage}/>
             </div>
         );
     }
