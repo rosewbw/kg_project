5c599165d80ad50f98cde6ca2b0b947eccf20aa4
DESKTOP-G2JJD5K\wbw <21680560@qq.com>
2018-05-20 23:16:50 +0800

    修改素材model中的name为title

--- server/database/models/mOnlineEditor.js
+++ server/database/models/mOnlineEditor.js
@@ -3,7 +3,7 @@ module.exports = {
         // 暂时使用默认 _id
         _id: {type: String, required: true},
         userId: {type: String, required: true},
-        name: {type: String, required: true},
+        title: {type: String, required: true},
         type: {type: String, required: true},
         keyword: {type: String, required: true},
         url: {type: String, required: true},
@@ -12,7 +12,6 @@ module.exports = {
         thumbnailUrl: {type: String, required: true},
         uniqueData: {type: Object},
         learningTime: {type: String},
-        title: {type: String},
         format: {type: String},
         comments: {type: Object},
         language: {type: String},
